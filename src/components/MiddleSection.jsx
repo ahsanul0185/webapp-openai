bb98db2ba8c903d5e55e53d5a7b3c56cbaa80c5f
ahsanul0185 <ahsanul0185@gmail.com>
2025-02-05 00:36:49 +0600

    615px heighy

--- src/components/MiddleSection.jsx
+++ src/components/MiddleSection.jsx
@@ -12,7 +12,7 @@ const MiddleSection = () => {
   return (
     <div className="flex-1 ">
 
-      <div className="px-6 sm:px-20 md:px-12 h-[642px] sm:h-full w-full overflow-y-scroll overflow-x-hidden custom-scrollbar lg:mt-10  lg:mb-[6rem]">
+      <div className="px-6 sm:px-20 md:px-12 h-[615px] sm:h-full w-full overflow-y-scroll overflow-x-hidden custom-scrollbar lg:mt-10  lg:mb-[6rem]">
       <div ref={topRef} />
         <p className="text-sm sm:text-base leading-[25px] sm:leading-loose sm:pt-16 lg:pt-0 sm:pb-[120px] lg:pb-[150px]">
           {selectedContent}
